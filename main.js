1dbbd8edace2f253491960bbf447e1cb9182b22e
prakashbhaskaran <prakashcsk2@gmail.com>
2024-12-23 13:06:53 +0530

    added project in portfolio

--- main.js
+++ main.js
@@ -8,7 +8,38 @@ const emailerror = document.querySelector(".emailerror");
 const uname = document.querySelector("#name");
 const unameerror = document.querySelector(".unameerror");
 const text = document.querySelector("#text");
+const portfolio = document.querySelector("#portfolio-container");
 const gracias = document.querySelector(".gracias");
+
+const portfolioList = [
+  { name: "Ecommerce Store", link: "https://kart.webibee.com" },
+  { name: "VS ANIMAL HEALTH", link: "https://vsanimalhealth.com" },
+  { name: "PLAINLIGHTS", link: "https://plainlights.com" },
+  { name: "OS CRANE PARTS", link: "https://oscraneparts.com" },
+  { name: "DYNAMAC DIGITAL", link: "https://dynamacdigital.vercel.app" },
+];
+
+portfolioList.forEach((item) => {
+  portfolio.innerHTML += `<div
+                            class="box"
+                            data-aos="zoom-in"
+                            data-aos-duration="1500"
+                            data-aos-once="true"
+                          >
+                          <img
+                            src="https://images.unsplash.com/photo-1536924940846-227afb31e2a5?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=747&q=80"
+                          />
+                          <h3>${item.name}</h3>
+                          <div class="icons">
+                            <a
+                              href=${item.link}
+                              class="fas fa-external-link-alt"
+                              target="_blank"
+                            ></a>
+                          </div>
+                        </div>`;
+});
+
 menu.onclick = () => {
   menu.classList.toggle("fa-times");
   header.classList.toggle("active");
